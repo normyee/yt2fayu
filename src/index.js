e97db6ecda940137d38ee8772db0b1a52e8e8ef0
normyee <lunoqro@gmail.com>
2025-07-27 22:16:12 -0300

    chore: small changes

--- src/index.js
+++ src/index.js
@@ -1,38 +1,30 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("node:path");
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 950,
     height: 500,
-    icon: path.join(__dirname, "assets", "hello-kitty-favicon.ico"),
+    resizable: false,
+    icon: path.join(__dirname, "assets", "my-melody-happy.ico"),
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
   });
 
-  // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, "index.html"));
   mainWindow.setMenu(null);
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
 
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
@@ -40,14 +32,10 @@ app.whenReady().then(() => {
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+//.\yt-dlp.exe -x --audio-format mp3 --yes-playlist "https://www.youtube.com/watch?v=oMfw5krsEd4&list=PL8eCIT_xYWGDBiavfpCEyF36DHbw_F7rm"
